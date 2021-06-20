7c456f89498571b8d3ca32c6a2b57e742ce6887e
kunal-j10 <kunalsinghlegal@gmail.com>
2021-06-20 19:27:49 +0530

    used class and objects

--- DSA/Array ADT/Missing_Elements_Unsorted_Array.cpp
+++ DSA/Array ADT/Missing_Elements_Unsorted_Array.cpp
@@ -1,30 +1,66 @@
-//  Find multiple missing elements in 
-//  an Unsorted array.
+// Finding missing elements in an array
+// E.g: A[10]={3,7,4,9,12,6,1,11,2,10}
+// missing elements = 5,8
 
-//         time complexity O(n)
+// Time Complexity of O(n)
 
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-void missing_elements(int* arr,int l)
+class General
 {
-    int brr[12]={0};
-    for(int i=0;i<l;i++)
+    public:
+    int length_of_array,max=0,min=0;
+    int* A;
+    
+    General()
     {
-        brr[arr[i]]++;
-    }
-    for(int i=0;i<12;i++)
-    {
-        if(brr[i]==0 && i>=3)
+        cout<<"Enter no. of elements in the array\n";
+        cin>>length_of_array;
+        A= new int(length_of_array);
+        cout<<"Enter array elements\n";
+        for(int i=0;i<length_of_array;i++)
         {
-            printf("%d\n",i);
+            cin>>A[i];
+        }
+        max=min=A[0];
+    }
+    
+    void Display()
+    {
+        cout<<"Displaying array elements  ";
+        for(int i=0;i<length_of_array;i++)
+        {
+            if(A[i]>=max)
+                max=A[i];
+            if(A[i]<=min)
+                min=A[i];
+            cout<<A[i]<<" ";
+        }
+        cout<<"\nmax = "<<max<<endl;
+        cout<<"min = "<<min<<endl;
+    }
+    void Hash()
+    {
+        int H[max+1]={0};
+        for(int i=0;i<length_of_array;i++)
+        {
+            H[A[i]]++;
+        }
+        for(int i=min;i<=max;i++)
+        {
+            if(H[i]==0)
+            {
+                cout<<i<<" ";
+            }
         }
     }
-}
+};
 
 int main()
 {
-    int arr[7]={3,10,4,5,11,7,12};
-    missing_elements(arr,7);
-    
+    General obj;          // O(n)
+    obj.Display();        // O(n)
+    obj.Hash();           // O(n)
 }
