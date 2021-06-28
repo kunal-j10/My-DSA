59de4ac3de841fbccb25c4ce8aab333472cd54c9
kunal-j10 <kunalsinghlegal@gmail.com>
2021-06-28 07:53:49 +0530

    Easy

---
+++ Array/Search Insert Position.cpp
@@ -0,0 +1,22 @@
+// time complexity = O(n)
+
+// Given a sorted array of distinct integers and a target value, 
+// return the index if the target is found. If not, return the index 
+// where it would be if it were inserted in order.
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n= nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>=target)
+            {
+                return i;
+            } 
+        }
+        return n;
+        
+    }
+};
