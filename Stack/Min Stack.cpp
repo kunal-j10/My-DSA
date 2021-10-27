ada252187f7364e83bbe1df9c0259248805dd6d0
kunal-j10 <kunalsinghlegal@gmail.com>
2021-10-27 09:39:36 +0530

    used STL Stack

---
+++ Stack/Min Stack.cpp
@@ -0,0 +1,47 @@
+# define N 10000
+class MinStack {
+    int* arr;
+    int* brr;
+    int ptr;
+public:
+    MinStack() {
+        arr= new int[N];
+        brr= new int[N];
+        ptr = -1;
+    }
+    
+    void push(int val) {
+        if(ptr<=N-1)
+        {
+            if(ptr==-1 || val<brr[ptr])
+            {
+                ptr++;
+                brr[ptr] = val;
+                arr[ptr]=val;
+            }
+            else if(val>=brr[ptr])
+            {
+                ptr++;
+                brr[ptr] = brr[ptr-1];
+                arr[ptr]=val;
+            }
+        }
+    }
+    
+    void pop() {
+        if(ptr!=-1)
+            ptr--;
+    }
+    
+    int top() {
+        if(ptr != -1)
+            return arr[ptr];
+        return 0;
+    }
+    
+    int getMin() {
+        if(ptr != -1)
+            return brr[ptr];
+        return 0;
+    }
+};
