626f07a115108f4ba76cbfe2205badea253439d2
kunal-j10 <kunalsinghlegal@gmail.com>
2021-10-28 09:58:01 +0530

    Basics

---
+++ Stack/Evaluation of Postfix Expression.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+    public:
+    
+    int evaluatePostfix(string S)
+    {
+        stack <int> st;
+        int i=0;
+        while(S[i]!='\0')
+        {
+            if(S[i]>='0' && S[i]<='9')
+            {
+                st.push(S[i]-'0');
+            }
+            else{
+                int op2=st.top();
+                st.pop();
+                int op1=st.top();
+                st.pop();
+                switch(S[i])
+                {
+                    case '+':{
+                        st.push(op1+op2);
+                        break;
+                    }
+                    case '-':{
+                        st.push(op1-op2);
+                        break;
+                    }
+                    case '*':{
+                        st.push(op1*op2);
+                        break;
+                    }
+                    case '/':{
+                        st.push(op1/op2);
+                        break;
+                    }
+                }
+            }
+            i++;
+        }
+        return st.top();
+    }
+};
