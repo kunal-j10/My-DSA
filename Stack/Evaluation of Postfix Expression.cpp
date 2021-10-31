da72e0e3b98daeb0515c8aa25eede69d409fc582
kunal-j10 <kunalsinghlegal@gmail.com>
2021-10-31 12:08:42 +0530

    Updated with Leetcode Solution

--- Stack/Evaluation of Postfix Expression.cpp
+++ Stack/Evaluation of Postfix Expression.cpp
@@ -1,3 +1,5 @@
+// # GFG Solution
+
 class Solution
 {
     public:
@@ -38,6 +40,69 @@ class Solution
                 }
             }
             i++;
+        }
+        return st.top();
+    }
+};
+
+
+
+
+// ##LeetCode Solution : Evaluate Reverse Polish Notation
+
+
+// Here we are given a vector of string
+// not just String ... so need to be carefull.
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        
+        // using while(tokens[i]!="\0") will give error
+        // as things are stored in vector so there is no \0
+        
+        
+        // point to remember : if "abc" 
+        //                     then tokens[0]= "abc"    -> string
+        //                     and  tokens[0][0] = 'a'  -> char
+        
+        
+        //                    also if "123"
+        //                    tokens[i]-'0'  will be wrong it equates to "123"-'0'
+        //                    stoi(token[i]) is correct 
+        
+        stack <int> st;
+        
+        for(int i=0;i<tokens.size();i++)
+        {
+            if(tokens[i]!= "+" && tokens[i]!= "-" && tokens[i]!= "*" && tokens[i]!= "/")
+                st.push(stoi(tokens[i]));
+            else{
+                int op2 = st.top();
+                st.pop();
+                int op1 = st.top();
+                st.pop();
+                switch(tokens[i][0])
+                {
+                    case '+':{
+                        st.push(op1 + op2);
+                        break;
+                    }
+                    case '-':{
+                        st.push(op1 - op2);
+                        break;
+                    }
+                    case '*':{
+                        st.push(op1 * op2);
+                        break;
+                    }
+                    case '/':{
+                        st.push(op1/op2);
+                        break;
+                    }
+                }
+            }
+            
         }
         return st.top();
     }
