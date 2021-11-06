8e941f855a38904f3ef019cf94fb5ce39c2cf94e
kunal-j10 <kunalsinghlegal@gmail.com>
2021-11-06 19:43:15 +0530

    Recursive + Iterative method

--- Binary Search Trees/Search a node in BST .cpp
+++ Binary Search Trees/Search a node in BST .cpp
@@ -9,3 +9,18 @@ bool search(struct Node* root, int x) {
     else if(root->data<x)
         return search(root->right,x);
 }
+
+// Iterative Method
+bool search(Node* root, int x) {
+    
+    while(root!=NULL)
+    {
+        if(root->data==x)
+            return 1;
+        else if(root->data>x)
+            root=root->left;
+        else
+            root=root->right;
+    }
+    return 0;
+}
