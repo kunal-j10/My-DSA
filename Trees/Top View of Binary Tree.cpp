81874517ce97547d8a2b8017bb6a68b557c99cea
kunal-j10 <kunalsinghlegal@gmail.com>
2021-12-21 22:03:42 +0530

    Easy

---
+++ Trees/Top View of Binary Tree.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+    vector<int> topView(Node *root)
+    {
+        queue<pair<struct Node*,int>> q;
+        map<int,int> mp;
+        struct Node* ptr=root;
+        q.push({ptr,0});
+        while(!q.empty())
+        {
+            auto p = q.front();
+            q.pop();
+            ptr=p.first;
+            int x=p.second;
+            
+            if(!mp[x])
+                mp[x]=ptr->data;
+            
+            if(ptr->left)
+                q.push({ptr->left,x-1});
+                
+            if(ptr->right)
+                q.push({ptr->right,x+1});
+            
+        }
+        vector<int> ans;
+        for(auto q : mp)
+        {
+            ans.push_back(q.second);
+        }
+        return ans;
+    }
+
+};
