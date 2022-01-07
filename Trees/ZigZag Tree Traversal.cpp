9ece76cfcc2137cd1e3e087f91a05910e825d41e
kunal-j10 <kunalsinghlegal@gmail.com>
2022-01-07 11:53:38 +0530

    Added Recursive method

--- Trees/ZigZag Tree Traversal.cpp
+++ Trees/ZigZag Tree Traversal.cpp
@@ -42,3 +42,54 @@ class Solution{
     	return v;
     }
 };
+
+
+// Recursive Approach
+
+class Solution{
+    public:
+    int height(Node* root)
+    {
+        if(root!=NULL)
+        {
+            int x= height(root->left);
+            int y= height(root->right);
+            return max(x,y)+1;
+        }
+        return 0;
+    }
+    void zzTransverse(Node* root,int height,bool lor,vector<int> &v)
+    {
+        if(height<=1)
+            v.push_back(root->data);
+        else
+        {
+            if(lor)
+            {
+                if(root->left)
+                    zzTransverse(root->left,height-1,lor,v);
+                if(root->right)
+                    zzTransverse(root->right,height-1,lor,v);
+            }
+            else{
+                if(root->right)
+                    zzTransverse(root->right,height-1,lor,v);
+                if(root->left)
+                    zzTransverse(root->left,height-1,lor,v);
+            }
+        }
+    }
+    
+    vector <int> zigZagTraversal(Node* root)
+    {
+    	int maxHeight = height(root);
+    	vector<int> v;
+    	bool ltor =1;
+    	for(int i=1;i<=maxHeight;i++)
+    	{
+    	    zzTransverse(root,i,ltor,v);
+    	    ltor=!ltor;
+    	}
+    	return v;
+    }
+};
