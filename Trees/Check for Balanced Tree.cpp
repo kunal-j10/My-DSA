57aab912e35f698af53d276d484757aff74437a4
kunal-j10 <kunalsinghlegal@gmail.com>
2022-01-07 19:33:20 +0530

    cleaned code

--- Trees/Check for Balanced Tree.cpp
+++ Trees/Check for Balanced Tree.cpp
@@ -1,30 +1,23 @@
 class Solution{
     public:
-    int height(struct Node* root)
+    int heightBalanced(Node* root)
     {
-        if(root==NULL)
-            return 0;
-        else{
-            int x = height(root->left);
-            int y = height(root->right);
-            if(x==-1 || y==-1)
-            {
-                return -1;
-            }
-            if(abs(x-y)>1)
+        if(root)
+        {
+            int x = heightBalanced(root->left);
+            int y= heightBalanced(root->right);
+            
+            if(x==-1 ||y==-1|| abs(x-y)>1)   // if this then not balanced
                 return -1;
             
-            if(x>=y)
-                return x+1;
-            else
-                return y+1;
+            return max(x,y)+1;               //returns height
         }
+        return 0;
     }
     bool isBalanced(Node *root)
     {
-        int x = height(root);
-        if(x==-1)
-            return 0;
-        return 1;
+        if(heightBalanced(root)==-1)
+            return false;
+        return true;
     }
 };
