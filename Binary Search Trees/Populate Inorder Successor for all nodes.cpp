4b74fb1f1f16ee10d3c6883607cd1a8e3599c6ce
kunal-j10 <kunalsinghlegal@gmail.com>
2022-01-12 23:32:24 +0530

    Easy

---
+++ Binary Search Trees/Populate Inorder Successor for all nodes.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    void Helper(Node *root,Node* &prev)
+    {
+        if(root)
+        {
+            Helper(root->left,prev);
+            if(prev!=NULL)
+                prev->next=root;
+            prev = root;
+            Helper(root->right,prev);
+        }
+        return;
+    }
+    void populateNext(Node *root)
+    {
+        Node* prev = NULL;
+        Helper(root,prev);
+    }
+};
