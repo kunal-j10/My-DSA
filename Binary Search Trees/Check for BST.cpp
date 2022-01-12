fbba29a9a81ba93fc4ca151623a0f6dbc6cc0af2
kunal-j10 <kunalsinghlegal@gmail.com>
2022-01-12 23:20:13 +0530

    optimised

--- Binary Search Trees/Check for BST.cpp
+++ Binary Search Trees/Check for BST.cpp
@@ -1,4 +1,4 @@
-void travel(Node* root,vector<int> &v)
+    void travel(Node* root,vector<int> &v)
     {
         if(root)
         {
@@ -17,3 +17,31 @@ void travel(Node* root,vector<int> &v)
         
         return temp==v;
     }
+
+
+
+
+// method 2:  without using extra space for vectors
+
+class Solution
+{
+    public:
+    void InorderTransversal(Node* root, int &prev,bool &ans)
+    {
+        if(root)
+        {
+            InorderTransversal(root->left,prev,ans);
+            if(prev >= root->data)
+                ans=0;
+            prev=root->data;
+            InorderTransversal(root->right,prev,ans);
+        }
+    }
+    bool isBST(Node* root) 
+    {
+        bool ans =1;
+        int prev= INT_MIN;
+        InorderTransversal(root,prev,ans);
+        return ans;
+    }
+};
