33ca58e926fffe4ea625aab0717bf130534c59ad
kunal-j10 <kunalsinghlegal@gmail.com>
2022-01-22 22:03:30 +0530

    Easy

---
+++ Binary Search Trees/Count BST nodes that lie in a given range.cpp
@@ -0,0 +1,13 @@
+int getCount(Node *root, int l, int h)
+{
+  if(root!= NULL)
+  {
+      int x= getCount(root->left,l,h);
+      int y= getCount(root->right,l,h);
+      if(root->data >=l && root->data <= h)
+            return x+y+1;
+      
+      return x+y;
+  }
+  return 0;
+}
