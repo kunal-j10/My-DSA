4f3164d5bc57da4c749a7bba98fe360f39c57a30
kunal-j10 <kunalsinghlegal@gmail.com>
2022-01-26 11:34:55 +0530

    Memoization + Tabulation

--- Dynamic Programming/Longest Common Subsequence.cpp
+++ Dynamic Programming/Longest Common Subsequence.cpp
@@ -1,3 +1,5 @@
+// method 1: Memoization
+
 class Solution
 {
     public:
@@ -25,5 +27,31 @@ class Solution
                 memo[i][j]=-1;
         }
         return LCS(x,y,s1,s2);
+    }
+};
+
+// method 2: Tabulation
+
+class Solution
+{
+    public:
+    int lcs(int x, int y, string s1, string s2)
+    {
+        int dp[x+1][y+1];
+        for(int i=0;i<=x;i++)
+            dp[i][0]=0;
+        for(int j=0;j<=y;j++)
+            dp[0][j]=0;
+        for(int i=1;i<=x;i++)
+        {
+            for(int j=1;j<=y;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[x][y];
     }
 };
