2fa5ca483be76f108b1f82f57448883c04f23f64
kunal-j10 <kunalsinghlegal@gmail.com>
2022-01-29 13:47:58 +0530

    Memoization + Tabulation

--- Dynamic Programming/0 - 1 Knapsack Problem .cpp
+++ Dynamic Programming/0 - 1 Knapsack Problem .cpp
@@ -21,3 +21,32 @@ class Solution
         return KnapS(W,wt,val,n);
     }
 };
+
+// Tabulation
+
+class Solution
+{
+    public:
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        int dp[n+1][W+1];
+        for(int i=0;i<=n;i++)
+            dp[i][0]=0;
+        for(int j=0;j<=W;j++)
+            dp[0][j]=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=W;j++)
+            {
+                if(j<wt[i-1])
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
+                }
+            }
+        }
+        return dp[n][W];
+    }
+};
