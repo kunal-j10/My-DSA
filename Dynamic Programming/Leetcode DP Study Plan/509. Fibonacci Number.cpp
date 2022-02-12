79c6c53566af1309fd2b01acd498389cca1a7e23
kunal-j10 <kunalsinghlegal@gmail.com>
2022-02-12 08:12:53 +0530

    Easy | Day-1

---
+++ Dynamic Programming/Leetcode DP Study Plan/509. Fibonacci Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int memo[31];
+    int fibonacci(int n)
+    {
+        if(n==0 || n==1)
+            return n;
+        if(memo[n]==-1)
+        {
+            return memo[n] = fibonacci(n-1) + fibonacci(n-2);
+        }
+        return memo[n];
+    }
+    int fib(int n) {
+        memset(memo,-1,sizeof(memo));
+        return fibonacci(n);
+    }
+};
