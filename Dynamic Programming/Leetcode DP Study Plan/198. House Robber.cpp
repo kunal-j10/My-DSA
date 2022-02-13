81dfb4d4594acc2abfa2e8431d7c943c930b1c0a
kunal-j10 <kunalsinghlegal@gmail.com>
2022-02-13 10:02:30 +0530

    Medium | Day-3

---
+++ Dynamic Programming/Leetcode DP Study Plan/198. House Robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int memo[102];
+    int helper(vector<int> nums,int n)
+    {
+        if(n<=0)
+            return 0;
+        if(memo[n]==-1)
+            return memo[n] = max(nums[n-1]+ helper(nums,n-2),helper(nums,n-1));
+        return memo[n];
+    }
+    int rob(vector<int>& nums) {
+        memset(memo,-1,sizeof(memo));
+        return helper(nums,nums.size());
+    }
+};
