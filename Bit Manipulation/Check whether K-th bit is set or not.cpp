08a9bb8ec35991e9c8b776120beeea9f7d9a95b6
kunal-j10 <kunalsinghlegal@gmail.com>
2022-03-04 08:26:30 +0530

    gfg Easy

---
+++ Bit Manipulation/Check whether K-th bit is set or not.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+    public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+        int x = 1<<k;
+        return (n&x);
+    }
+};
